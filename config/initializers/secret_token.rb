91879b0b1abdd00fa01af7bc02b205a3a271dd00
James Maniscalco <james@James-MBP.local>
2012-11-11 14:34:06 -0500

    initial commit of user_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserApp::Application.config.secret_token = 'd12ec037f8e39f24b374659b163557d3b97f035783c162ff9e8b82695a94b1cf8d2290f218a54f72c87454063502dde3a0a8f619fd0d02a87a14031f0f873ee7'
